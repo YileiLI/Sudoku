e1620614b4cd32d0e57da0eebd3fd9e95ef45aa8
李怡蕾 <Bullshit@abosolutelyl.local>
2017-06-13 09:49:45 +0200

    Initial commit

---
+++ app/src/main/java/org/example/sudokut/MainActivity.java
@@ -0,0 +1,12 @@
+package org.example.sudokut;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
